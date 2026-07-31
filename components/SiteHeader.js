6b60aa44d7d3852843ea6557df9a817ef536efc7
JorisMB82 <joris@jvlimited.com>
2026-07-30 22:16:16 -0400

    Add Maple Map navigation

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation';
 
 const links = [
   ['/explorer', 'Explorer'],
+  ['/map', 'Map'],
   ['/compare', 'Compare'],
   ['/graph', 'Graph'],
   ['/editorial', 'Editorial'],
